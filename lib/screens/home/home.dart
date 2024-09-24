42176160d19e27fc4db2093c7fd207e4b3c6c7c5
ZakariaAshraf <145775945+ZakariaAshraf@users.noreply.github.com>
2024-09-24 17:48:07 +0300

    make a card widget for the one note UI.and display it in a grid view builder implement a fire store get data method,and make this work with currently data i added.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,9 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+   HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+
+
+}
+class _HomePageState extends State<HomePage> {
+  List<QueryDocumentSnapshot> data=[];
+@override
+  void initState() {
+    // TODO: implement initState
+  getData();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +35,31 @@ class HomePage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(onPressed: () {
         Navigator.pushNamed(context, "AddPage");
       },child: Icon(Icons.add,),),
+      body: GridView.builder(
+        itemCount: data.length,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemBuilder: (BuildContext context, int index) {
+          return  Card(
+            child: Container(
+              child: Column(
+                children: [
+                  Icon(Icons.note_rounded,size: 100,color: Colors.grey,),
+                  Text("${data[index]["name"]}"),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
 
     );
+
   }
+   getData()async{
+     QuerySnapshot response=await FirebaseFirestore.instance.collection("categories").get();
+     data.addAll(response.docs);
+     setState(() {});
+   }
+
+
 }
