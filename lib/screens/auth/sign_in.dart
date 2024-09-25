aa00aab8822847158b9deb08c3201f2bddf6aac8
ZakariaAshraf <145775945+ZakariaAshraf@users.noreply.github.com>
2024-09-25 03:41:00 +0300

    add and learn about new package to use name awesome_dialog build a fire base delete method and use it in my app

--- lib/screens/auth/sign_in.dart
+++ lib/screens/auth/sign_in.dart
@@ -1,3 +1,4 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mastering_firebase/screens/auth/sign_up.dart';
@@ -12,6 +13,7 @@ class SignIn extends StatefulWidget {
 class _SignInState extends State<SignIn> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -20,134 +22,145 @@ class _SignInState extends State<SignIn> {
         title: const Text("Sign In Page"),
         centerTitle: true,
       ),
-      body: ListView(
-        children: [
-          const ImageIcon(
-            AssetImage("assets/images/otp.png"),
-            size: 100,
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          TextFormField(
-            controller: emailController,
-            decoration: InputDecoration(
-              label: const Text("Email"),
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(
-                20,
-              )),
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          TextFormField(
-              controller: passwordController,
-              decoration: InputDecoration(
-                label: const Text("Password"),
-                border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(
-                  20,
-                )),
-              )),
-          InkWell(
-              onTap: () async {
-                try {
-                  await FirebaseAuth.instance
-                      .sendPasswordResetEmail(email: emailController.text);
-                  AlertDialog(
-                    title:
-                        Text("We sent a reset email ,please check your inbox"),
-                  );
-                } catch (e) {
-                  // TODO
-                  print(e.toString());
-                }
-              },
-              child: Container(
-                child: Text(
-                  "Forget Password ?",
-                  style: TextStyle(
-                    color: Colors.deepOrange,
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView(
+              children: [
+                const ImageIcon(
+                  AssetImage("assets/images/otp.png"),
+                  size: 100,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    label: const Text("Email"),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(
+                      20,
+                    )),
                   ),
                 ),
-                alignment: Alignment.topRight,
-                margin: EdgeInsets.all(10),
-              )),
-          const SizedBox(
-            height: 30,
-          ),
-          Center(
-            child: GestureDetector(
-              onTap: () async {
-                try {
-                  final userSigned = await FirebaseAuth.instance
-                      .signInWithEmailAndPassword(
-                          email: emailController.text,
-                          password: passwordController.text);
-                  Navigator.pushNamedAndRemoveUntil(
-                    context,
-                    "HomePage",
-                    (route) => false,
-                  );
-                } on FirebaseAuthException catch (e) {
-                  // TODO
-                  if (e.code == 'user-not-found') {
-                    print('No user found for that email.');
-                  } else if (e.code == 'wrong-password') {
-                    print('Wrong password provided for that user.');
-                  }
-                }
-              },
-              child: Container(
-                height: 50,
-                width: 200,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: Colors.redAccent,
+                const SizedBox(
+                  height: 30,
                 ),
-                child: const Center(
-                  child: Text(
-                    'Sign in',
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold),
-                  ),
+                TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      label: const Text("Password"),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(
+                        20,
+                      )),
+                    )),
+                InkWell(
+                    onTap: () async {
+                      try {
+                        await FirebaseAuth.instance.sendPasswordResetEmail(
+                            email: emailController.text);
+                        AwesomeDialog(
+                            animType: AnimType.leftSlide,
+                            context: context,
+                            dialogType: DialogType.warning,
+                            btnOkOnPress: () {},
+                            title: "Warning",
+                            desc: "we sent you a verification email. ").show();
+                      } catch (e) {
+                        // TODO
+                        print(e.toString());
+                      }
+                    },
+                    child: Container(
+                      child: Text(
+                        "Forget Password ?",
+                        style: TextStyle(
+                          color: Colors.deepOrange,
+                        ),
+                      ),
+                      alignment: Alignment.topRight,
+                      margin: EdgeInsets.all(10),
+                    )),
+                const SizedBox(
+                  height: 30,
                 ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 30,
-          ),
-          Row(
-            children: [
-              Text("Don't have an account",
-                  style: TextStyle(
-                    color: Colors.blueAccent,
-                  )),
-              TextButton(
-                onPressed: () {
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SignUp(),
+                Center(
+                  child: GestureDetector(
+                    onTap: () async {
+                      try {
+                        bool isLoading = true;
+                        setState(() {});
+                        final userSigned = await FirebaseAuth.instance
+                            .signInWithEmailAndPassword(
+                                email: emailController.text,
+                                password: passwordController.text);
+                        isLoading = false;
+                        setState(() {});
+                        Navigator.pushNamedAndRemoveUntil(
+                          context,
+                          "HomePage",
+                          (route) => false,
+                        );
+                      } on FirebaseAuthException catch (e) {
+                        // TODO
+                        if (e.code == 'user-not-found') {
+                          print('No user found for that email.');
+                        } else if (e.code == 'wrong-password') {
+                          print('Wrong password provided for that user.');
+                        }
+                      }
+                    },
+                    child: Container(
+                      height: 50,
+                      width: 200,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.redAccent,
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Sign in',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
                     ),
-                  );
-                },
-                child: const Text(
-                  "Create!",
-                  style: TextStyle(
-                    color: Colors.redAccent,
                   ),
                 ),
-              ),
-            ],
-          ),
-        ],
-      ),
+                const SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  children: [
+                    Text("Don't have an account",
+                        style: TextStyle(
+                          color: Colors.blueAccent,
+                        )),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => SignUp(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        "Create!",
+                        style: TextStyle(
+                          color: Colors.redAccent,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
     );
   }
 }
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,23 +1,26 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-   HomePage({Key? key}) : super(key: key);
+  HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
-
-
 }
+
 class _HomePageState extends State<HomePage> {
-  List<QueryDocumentSnapshot> data=[];
-@override
+  bool isLoading = true;
+  List<QueryDocumentSnapshot> data = [];
+
+  @override
   void initState() {
     // TODO: implement initState
-  getData();
+    getData();
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,39 +30,82 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Colors.red,
         actions: [
-          IconButton(onPressed: ()async{
-            await FirebaseAuth.instance.signOut();
-          }, icon: Icon(Icons.logout_outlined),),
+          IconButton(
+            onPressed: () async {
+              try {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushNamedAndRemoveUntil(
+                  context,
+                  "SignInPage",
+                  (route) => false,
+                );
+              } catch (e) {
+                // TODO
+                print(e.toString());
+              }
+            },
+            icon: Icon(Icons.logout_outlined),
+          ),
         ],
       ),
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        Navigator.pushNamed(context, "AddPage");
-      },child: Icon(Icons.add,),),
-      body: GridView.builder(
-        itemCount: data.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        itemBuilder: (BuildContext context, int index) {
-          return  Card(
-            child: Container(
-              child: Column(
-                children: [
-                  Icon(Icons.note_rounded,size: 100,color: Colors.grey,),
-                  Text("${data[index]["name"]}"),
-                ],
-              ),
-            ),
-          );
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pushNamed(context, "AddPage");
         },
+        child: Icon(
+          Icons.add,
+        ),
       ),
-
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : GridView.builder(
+              itemCount: data.length,
+              gridDelegate:
+                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  onDoubleTap: () => AwesomeDialog(
+                    animType: AnimType.rightSlide,
+                    context: context,
+                    dialogType: DialogType.warning,
+                    btnCancelOnPress: () {},
+                    btnOkOnPress: () async {
+                      await FirebaseFirestore.instance
+                          .collection("categories")
+                          .doc(data[index].id)
+                          .delete();
+                      Navigator.pushReplacementNamed(context, "HomePage");
+                    },
+                    title: "Warning",
+                    desc: "are you sure delete",
+                  ).show(),
+                  child: Card(
+                    child: Container(
+                      child: Column(
+                        children: [
+                          Icon(
+                            Icons.note_rounded,
+                            size: 100,
+                            color: Colors.grey,
+                          ),
+                          Text("${data[index]["name"]}"),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
     );
-
   }
-   getData()async{
-     QuerySnapshot response=await FirebaseFirestore.instance.collection("categories").get();
-     data.addAll(response.docs);
-     setState(() {});
-   }
 
-
+  getData() async {
+    QuerySnapshot response =
+        await FirebaseFirestore.instance.collection("categories").get();
+    data.addAll(response.docs);
+    isLoading = false;
+    setState(() {});
+  }
 }
